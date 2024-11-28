97637df58f491469e772a5c22ea449a2346d520e
KisnaGupta <150457131+KisnaGupta@users.noreply.github.com>
2024-11-28 15:01:46 +0530

    Add files via upload

---
+++ DSA!/problem16.cpp
@@ -0,0 +1,28 @@
+// 16. Function to print a pattern with increasing and fixed numbers.
+#include <stdio.h>
+
+void printPattern(int rows) {
+    for (int i = 0; i < rows; i++) {
+        
+        for (int j = i + 1; j <= rows; j++) {
+            printf("%d", j);
+        }
+        
+        for (int k = 0; k < i; k++) {
+            printf("%d", rows);
+        }
+        
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printPattern(rows);
+
+    return 0;
+}
